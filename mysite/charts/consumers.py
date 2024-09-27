78a5a7aa14bf28ea61da8839ff7554a71f0daeb2
yilin <ch.yilin@icloud.com>
2024-09-27 10:07:40 +0200

    Moved conversion of data and time to the server.

--- mysite/charts/consumers.py
+++ mysite/charts/consumers.py
@@ -2,6 +2,7 @@ import json
 import asyncio
 import websockets
 import logging
+from datetime import datetime, timezone
 
 from channels.generic.websocket import AsyncWebsocketConsumer
 
@@ -21,11 +22,36 @@ class BridgeConsumer(AsyncWebsocketConsumer):
     async def disconnect(self, close_code):
         await self.ros_bridge.close()
 
+    def rad_to_deg(self, rad):
+        return rad * (180 / 3.14159265)
+    
+    #  convert radians to degrees
+    def convert_data(self, data):
+        return [
+            self.rad_to_deg(val) if idx not in [3, 4, 5] else val
+            for idx, val in enumerate(data)
+        ]
+    
+    def convert_timestamp(self, timestamp):
+        dt = datetime.fromtimestamp(timestamp, tz=timezone.utc)
+        return dt.strftime('%H:%M:%S')
+
     async def forward_data(self):
         logger.debug("forwarding data")
         while True:
             message = await self.ros_bridge.recv()
             logger.debug(f"Received message from ROS: {message}")
-            await self.send(text_data=message)
+            data = json.loads(message)
+            if 'msg' in data:
+                if 'data' in data['msg']:
+                    # Convert the data
+                    converted_data = self.convert_data(data['msg']['data'])
+                    data['msg']['data'] = converted_data
+                if 'time' in data['msg']:
+                    # Convert the timestamp
+                    converted_time = self.convert_timestamp(data['msg']['time'])
+                    data['msg']['time'] = converted_time
+
+            await self.send(text_data=json.dumps(data))
 
 
